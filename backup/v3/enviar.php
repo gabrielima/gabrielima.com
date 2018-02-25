a8745f793eeee2910021819766e8c60ae05d5201
Gabriel de Lima Rabelo <gabriellima_r@hotmail.com>
2018-02-25 11:10:04 -0300

    remove backup

--- backup/v3/enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$nome     = $_POST['nome'];
-	$email    = $_POST['email'];
-	$assunto  = $_POST['assunto'];
-	$mensagem = $_POST['msg'];
-
-	mail("gabriellima_r@hotmail.com","$assunto","$mensagem","FROM:$nome<$email>");
-?>
