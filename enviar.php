99ebc618a6b31ede0989e58127a7c2b22831596c
gabrielima <gabriellima_r@hotmail.com>
2018-03-11 23:28:46 -0300

    backup v2

--- enviar.php
+++ enviar.php
@@ -1,15 +1,26 @@
 <?php
 
-	$nome=$_POST['nome'];
-	$email=$_POST['email'];
-	$assunto=$_POST['assunto'];
-	$mensagem=$_POST['mensagem'];
+  $nome=$_POST['nome'];
+  $email=$_POST['email'];
+  $assunto=$_POST['assunto'];
+  $mensagem=$_POST['mensagem'];
 
-	 mail("gabriellima_r@hotmail.com","$assunto","
-	 Nome: $nome
-	 Email: $email
-	 Assunto: $assunto
-	 Mensagem: $mensagem","FROM:$nome<$email>");
+  $ch = curl_init();
 
-	 echo "<script>alert("a");</script>";
-?>
+  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  // Colocar a API key aqui
+  curl_setopt($ch, CURLOPT_USERPWD, 'api:key-6nu71dw--i4ey50558y-djc7qak2l702');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  // Colocar a url que o mailgun te oferece entre /v2/ .. /messages
+  curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/api.mailgun.net/v2/messages');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, array('from' => '$nome, $email',
+                                             'to' => 'gabriellima_r@hotmail.com',
+                                             'subject' => $assunto,
+                                             'text' => $mensagem, 
+                                             ));
+
+  $result = curl_exec($ch);
+  curl_close($ch);
+>
