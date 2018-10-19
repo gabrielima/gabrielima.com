6d04ab77329b1d05b8c1dcb0fb13c9c979df8e38
gabrielima <gabriellima_r@hotmail.com>
2018-10-19 14:32:54 -0300

    main components

---
+++ components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer__copy">Copyright 2018 - Gabriel Lima</p>
+
+    <style jsx>{`
+      .footer {
+        background-color: #eceeef;
+        text-align: center;
+        padding: 15px 0;
+      }
+    `}</style>
+  </footer>
+);
+
+export default Footer;
