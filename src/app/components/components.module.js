ba85243068003cd637631131b329a12b2ecdf339
gabrielima <gabriellima_r@hotmail.com>
2018-10-19 14:30:07 -0300

    cleanup to start a new structure

--- src/app/components/components.module.js
+++
@@ -1,4 +0,0 @@
-angular
-  .module('app.components', [
-    'app.common'
-  ]);
--- src/app/components/projects/projects.controller.js
+++
@@ -1,7 +0,0 @@
-angular
-  .module('app.components')
-  .controller('ProjectsController', ProjectsController);
-
-function ProjectsController() {
-  var ctrl = this;
-}
