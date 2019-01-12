221727159a798f80364471fdb089cc0bc85b5c9f
gabrielima <gabriellima_r@hotmail.com>
2019-01-12 18:15:24 -0200

    skills new layout

--- app/src/pages/home/components/Skills/SkillsStyles.js
+++ app/src/pages/home/components/Skills/SkillsStyles.js
@@ -1,20 +1,49 @@
 import styled from 'styled-components';
+import media from './../../../../styles/utils/media';
 
-const SkillsList = styled.ul`
+const SkillsContainer = styled.div`
+`;
+
+SkillsContainer.Area = styled.div`
   display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+
+  ${media.sm`
+    flex-direction: row;
+  `}
+`;
+
+SkillsContainer.Area_Name = styled.h4`
+  margin: 0 0 1rem;
+  
+  ${media.sm`
+    margin: 0;
+    display: flex;
+    flex: 0 0 150px;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 30px;
+  `}
+`;
+
+SkillsContainer.List = styled.ul`
+  display: flex;
+  justify-content: flex-start;
   align-items: center;
-  justify-content: center;
   flex-wrap: wrap;
   list-style: none;
   padding: 0;
   margin: 0;
 `;
 
-SkillsList.Item = styled.li`
+SkillsContainer.List_Item = styled.li`
   display: inline-block;
-  padding: 10px 15px;
+  padding: 6px 15px;
   border-radius: 10px;
   border: 1px solid;
+  font-size: 0.7rem;
+  margin: 4px;
 `;
 
-export default SkillsList;
+export default SkillsContainer;
--- app/src/pages/home/components/Skills/index.js
+++ app/src/pages/home/components/Skills/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 import HomeSection from '../../HomeStyles';
-import SkillsList from './SkillsStyles';
+import SkillsContainer from './SkillsStyles';
 
 const Skills = ({ skills }) => (
   <HomeSection>
     <HomeSection.Title>Skills</HomeSection.Title>
 
-    <SkillsList>
-      {skills && skills.map((skill, index) => (
-        <SkillsList.Item key={index}>{skill}</SkillsList.Item>
+    <SkillsContainer>
+      {skills && skills.map((area, index) => (
+        <SkillsContainer.Area>
+          <SkillsContainer.Area_Name>
+            <span>{area.area}:</span>
+          </SkillsContainer.Area_Name>
+          
+          <SkillsContainer.List>
+            {area.skills.map((skill, index) => (
+              <SkillsContainer.List_Item key={index}>{skill}</SkillsContainer.List_Item>
+            ))}
+          </SkillsContainer.List>
+        </SkillsContainer.Area>
       ))}
-    </SkillsList>
+    </SkillsContainer>
   </HomeSection>
 );
 
