9f667232bee93f13fd70517ff87ead8e4df1098f
gabrielima <gabriellima_r@hotmail.com>
2019-01-12 17:19:20 -0200

    about section

---
+++ app/src/pages/home/components/About/AboutStyles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import media from './../../../../styles/utils/media';
+import theme from './../../../../styles/theme';
+
+const AboutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  ${media.sm`
+    flex-direction: row;
+  `}
+`;
+
+AboutContainer.Child = styled.div`
+  :last-child {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px 0;
+  }
+
+  ${media.sm`
+    padding: 20px;
+  `}
+`;
+
+AboutContainer.Image = styled.img`
+  width: 120px;
+
+  ${media.sm`
+    width: 200px;
+  `}
+`;
+
+AboutContainer.Bio = styled.p`
+  font-size: 1rem;
+  line-height: 1.3;
+`;
+
+AboutContainer.Link = styled.a`
+  display: inline-block;
+  padding: 7px 20px;
+  border: 1px solid;
+  border-radius: 10px;
+  text-decoration: none;
+  width: 100px;
+  color: ${theme.colors.default};
+  transition: background-color .4s ease;
+
+  :hover {
+    background-color: ${theme.colors.primary};
+    color: #fff;
+  }
+`;
+
+export default AboutContainer;
--- app/src/pages/home/components/About/index.js
+++ app/src/pages/home/components/About/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 import HomeSection from '../../HomeStyles';
+import AboutContainer from './AboutStyles';
 
 const About = ({ bio }) => (
   <HomeSection>
     <HomeSection.Title>About</HomeSection.Title>
 
-    <div>
-      <img src="/static/img/gabrielima.jpg" alt="Gabriel Lima" />
+    <AboutContainer>
+      <AboutContainer.Child>
+        <AboutContainer.Image src="/static/img/gabrielima.jpg" alt="Gabriel Lima" />
+      </AboutContainer.Child>
 
-      <p>{bio}</p>
+      <AboutContainer.Child>
+        <AboutContainer.Bio>
+          { bio }
+        </AboutContainer.Bio>
 
-      <a
-        href="/static/resume.pdf"
-        target="_blank"
-        rel="noopener nofollow">
-        Resume
-      </a>
-    </div>
+        <AboutContainer.Link href="/static/resume.pdf" target="_blank" rel="noopener nofollow">
+          Resume
+        </AboutContainer.Link>
+      </AboutContainer.Child>
+    </AboutContainer>
   </HomeSection>
 );
 
