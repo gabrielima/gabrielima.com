2a75c81e8be17d7a5659311c97e43db4c24569b6
gabrielima <gabriellima_r@hotmail.com>
2019-01-12 19:42:18 -0200

    blog page done

--- app/src/pages/blog/index.js
+++ app/src/pages/blog/index.js
@@ -4,6 +4,7 @@ import React, { Fragment } from 'react';
 import Head from '../../components/Head';
 import { formatDate } from './../../misc/date-formatter';
 import { getPosts } from './../../services/posts';
+import BlogContainer from './BlogStyles';
 
 const Post = ({ post }) => (
   <div>
@@ -18,10 +19,12 @@ const Blog = ({ posts }) => (
   <Fragment>
     <Head title="Blog" />
 
-    <section>
-      <h1>Posts</h1>
-      {posts.map((post, index) => <Post post={post} key={index} />)}
-    </section>
+    <BlogContainer>
+      <BlogContainer.Title>Blog</BlogContainer.Title>
+      {/* {posts.map((post, index) => <Post post={post} key={index} />)} */}
+
+      <p style={{'marginTop': '5rem'}}>Work in progress.</p>
+    </BlogContainer>
   </Fragment>
 );
 
