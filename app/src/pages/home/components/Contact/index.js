d4ca11d232d144cf0e52df39889feef0a4e90d89
gabrielima <gabriellima_r@hotmail.com>
2019-01-12 18:14:34 -0200

    contact button

--- app/src/pages/home/components/Contact/index.js
+++ app/src/pages/home/components/Contact/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import HomeSection from '../../HomeStyles';
+import ContactContainer from './ContactStyles';
 
 const Contact = ({ email }) => (
   <HomeSection>
     <HomeSection.Title>Contact</HomeSection.Title>
 
-    <a href={`mailto:${email}`}>{ email }</a>
+    <ContactContainer.Link href={`mailto:${email}`}>
+      { email }
+    </ContactContainer.Link>
   </HomeSection>
 );
 
