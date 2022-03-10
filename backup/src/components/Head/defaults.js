bb8a90ae1e0c70229ce5319bde5c5a27ac8c45eb
Gabriel <rabeloglr@gmail.com>
2022-03-10 15:30:13 -0300

    remove backup

--- backup/src/components/Head/defaults.js
+++
@@ -1,9 +0,0 @@
-const defaults = { 
-  keywords: ['gabriel lima', 'front end', 'angular', 'web design', 'full stack', 'javascript'],
-  description: 'Gabriel Lima - Full Stack Developer',
-  ogImage: 'http://www.gabrielima.com/static/img/gabrielima.jpg',
-  url: 'http://www.gabrielima.com',
-  title: 'Gabriel Lima - Full Stack Developer'
-};
-
-export default defaults;
--- backup/src/styles/theme/default.js
+++
@@ -1,9 +0,0 @@
-const defaultTheme = {
-  colors: {
-    default: '#444',
-    primary: '#22A7F0',
-    primary_active: '#0d5d8a'
-  }
-};
-
-export default defaultTheme;
